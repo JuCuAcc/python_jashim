a11df334d3f4019d50891a98978673902eb7a416
MD. Jashim Uddin <ju.cu.acc@outlook.com>
2022-10-27 18:27:18 +0600

    first

---
+++ python_basic/python_basic.py
@@ -0,0 +1,45 @@
+
+##Initial
+
+#print("Jashim Uddin")
+#msg = "Assalamu Alaikum"
+#print(msg)
+#first_name = "Jashim"
+#last_name = "Uddin"
+#full_name = first_name + ' ' + last_name
+#print(full_name)
+
+##Lists
+#bikes = ['trek', 'redline', 'giant']
+#first_bike = bikes[0]
+#last_bike = bikes[-1]
+#print(first_bike)
+#print(last_bike)
+
+##Looping through list
+#for bike in bikes:
+#    print(bike)
+
+#bikes = []
+#bikes.append('trek')
+#bikes.append('redline')
+#bikes.append('giant')
+
+##Looping through list
+#for bike in bikes:
+#    print(bike)
+
+##Making numerical lists
+#squares = []
+#for x in range(1,11):
+#    squares.append(x**2)
+#print(squares)
+
+##List Comprehensions
+#squares = [x**2 for x in range(1,11)]
+#for square in squares:
+#    print(square)
+
+#Slicing a list
+finishers = ['sam', 'bob', 'ada', 'bea']
+
