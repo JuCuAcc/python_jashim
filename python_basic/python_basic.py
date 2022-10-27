9b6723138511a39496b97d2e7471e2c831683c6c
MD. Jashim Uddin <ju.cu.acc@outlook.com>
2022-10-27 18:58:30 +0600

    User Input

--- python_basic/python_basic.py
+++ python_basic/python_basic.py
@@ -40,6 +40,65 @@
 #for square in squares:
 #    print(square)
 
-#Slicing a list
-finishers = ['sam', 'bob', 'ada', 'bea']
+##Slicing a list
+#finishers = ['sam', 'bob', 'ada', 'bea']
+#first_two = finishers[:2]
+#print(first_two)
 
+##Copying a list
+#bikes = ['trek', 'redline', 'giant']
+
+##Boolean
+#'trek' in bikes
+#'surly' not in bikes 
+#game_active = True
+#can_edit = False
+
+#copy_of_bikes = bikes[:]
+#print(copy_of_bikes)
+
+##Tuples are similar to lists, but the items in atuple can't be modified
+##Making a tuple
+#dimensions =(1920, 1080)
+#print(dimensions)
+
+
+##If statements
+#age = 20
+#if age >= 18:
+#    print("You can vote!")
+
+##if-elif-else statements
+#age = 10
+#if age < 4:
+#    ticket_price = 0
+#elif age < 18:
+#    ticket_price = 10
+#else:
+#    ticket_price = 15
+#print(ticket_price)
+
+##Dictionaries
+#alien = {'color': 'green', 'points': 5}
+#print("The alien's color is " + alien['color'])
+#alien['x_position'] = 0
+##Looping through all key-value pairs
+#fav_numbers = {'eric': 17, 'ever': 4}
+#for name in fav_numbers.keys():
+#    print(name + ' loves a number')
+##Looping through all values
+#fav_numbers = {'eric': 17, 'ever': 4}
+#for number in fav_numbers.values():
+#    print(str(number) + ' is a favorite')
+
+##User input
+
+#name = input("What's your name? \n")
+#print("Hello, " + name + "!")
+#age = input("How old are you? \n")
+#age = int(age)
+#print("You are " + str(age) + " years old. ")
+
+#pi = input("What's the value of pi? ")
+#pi = float(pi)
+#print(pi)
