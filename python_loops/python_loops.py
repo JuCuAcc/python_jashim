52f1442b8ca73d49429ed479ad5d8946add0d3f4
MD. Jashim Uddin <ju.cu.acc@outlook.com>
2022-10-28 19:48:15 +0600

    infinite

--- python_loops/python_loops.py
+++ python_loops/python_loops.py
@@ -81,4 +81,102 @@
 #else:
 #    print("We have no players yet!")
 
+## Accepting input
 
+#name = input("What's your name? ")
+#print("Hello, " + name + ".")
+#age = input("How old are you? ")
+#age = int(age)
+
+#if age >= 18:
+#    print("\nYou can vote!")
+#else:
+#    print("\nYou can't vote yet.")
+
+##Python 2.7
+#name = raw_input("What's your name? ")
+#print("Hello, " + name + ".")
+
+
+## While loops
+
+#current_number = 1
+#while current_number <= 5: 
+#    print(current_number) 
+#    current_number += 1
+
+
+## Letting the user choose when to quit
+
+#prompt = "\nTell me something, and I'll "
+#prompt += "repeat it back to you."
+#prompt += "\nEnter 'quit' to end the program. "
+
+#message = ""
+#while message != 'quit':
+#    message = input(prompt)
+
+#    if message != 'quit':
+#        print(message)
+
+## Using a flag
+#active = True
+#while active:
+#    message = input(prompt)
+
+#    if message == 'quit':
+#        active = False
+#    else:
+#        print(message)
+
+# Using break to exit a loop
+prompt = "\nWhat cities have you visited?"
+prompt += "\nEnter 'quit' when you're done."
+
+while True:
+    city = input(prompt)
+
+    if city == 'quit':
+        break
+    else:
+        print("I've been to " + city + "!")
+
+# You can use continue to skip over certain items
+#   when looping through a list or dictionary as well.
+
+banned_users = ['eve', 'fred', 'gary', 'helen']
+
+prompt = "\nAdd a player to your team."
+prompt += "\nEnter 'quit' when you're done. "
+
+players = []
+while True: 
+    player = input(prompt)
+    if player == 'quit':
+        break
+    elif player in banned_users:    
+        print(player + " is banned!")
+        continue
+    else:
+        players.append(player)
+
+print("\nYour team:")
+for player in players:
+    print(player)
+
+## An infinite loop
+#while True:
+#    name = input("\nWho are you?")
+#    print("Nice to meet you, " + name + "!")
+
+
+# The remove() method removes a specific value from a list,
+#   but it only removes the first instance of the value you provide.
+#       You can use a while loop to remove all instances of a particular value.
+
+# Removing all cats from a list of pets
+pets = ['dog', 'cat', 'dog', 'fish', 'cat', 'rabbit', 'cat']
+print(pets)
+while 'cat' in pets:
+    pets.remove('cat')
+print(pets)
