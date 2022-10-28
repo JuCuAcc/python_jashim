f935a0faeff3ddd50f2e3c40f910be5ea3f164aa
MD. Jashim Uddin <ju.cu.acc@outlook.com>
2022-10-28 19:16:36 +0600

    loop

---
+++ python_loops/python_loops.py
@@ -0,0 +1,84 @@
+
+
+#car = 'bmw'
+#print(car == 'bmw')
+#car = 'audi'
+#print(car == 'bmw')
+#print(car.lower() == 'audi')
+
+#topping = 'mushrooms'
+#print(topping != 'anchovies')
+
+## Equality, Inequality and Comparison Operators
+#age = 18
+#age == 18
+#age != 18
+#age = 19
+#age < 21
+#age <= 21
+#age > 21
+#age >= 21
+
+## Multiple conditions
+#age_0 = 22
+#age_1 = 18
+
+#print(age_0 >= 21 and age_1 >= 21)
+#age_1 = 23
+#print(age_0 >= 21 and age_1 >= 21)
+
+#print(age_0 >= 21 or age_1 >= 21)
+#age_1 = 18
+#print(age_0 >= 21 or age_1 >= 21)
+
+## Boolean
+
+#game_active = True
+#can_edit = False
+
+
+## If statement
+
+#age = 19
+#if age >= 18:
+#    print("You're old enough to vote!")
+
+#age = 17
+#if age >= 18:
+#    print("You're old enough to vote!")
+#else:
+#    print("You can't vote yet.")
+
+
+#age = 12
+#if age < 4:
+#    price = 0
+#elif age < 18:
+#    price = 5
+#else:
+#    price = 10
+
+#print("Your cost is $" + str(price) + ".")
+
+
+#players = ['al', 'bea', 'cyn', 'dale']
+#print('al' in players)
+#print('eric' in players)
+
+#banned_users = ['and', 'chad', 'dee']
+#user = 'erin'
+
+#if user not in banned_users:
+#    print("You can play!")
+
+
+#players = []
+
+#if players:
+#    for player in players:
+#        print("Player: " + player.title())
+
+#else:
+#    print("We have no players yet!")
+
+
