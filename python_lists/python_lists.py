0cfdfd2cf6e9b40d0c375c6cb65dfb71f1b5ca52
MD. Jashim Uddin <ju.cu.acc@outlook.com>
2022-10-28 10:27:20 +0600

    Readbility

--- python_lists/python_lists.py
+++ python_lists/python_lists.py
@@ -83,7 +83,7 @@
 #for user in users:
 #    print(user)
 
-##Printing a message for each item, and aseparate message afterwards
+##Printing a message for each item, and a separate message afterwards
 #for user in users: 
 #    print("Welcome, " + user + "!")
 #print("Welcome, we're glad to see you all!")
@@ -99,4 +99,84 @@
 ##Making a list of numbers from 1 to a million
 #numbers = list(range(1, 1000001))
 
+##Simple statistics
+#ages = [93, 99, 66, 17, 85, 1, 35, 82, 2, 77]
+#youngest = min(ages)
+#oldest = max(ages)
+#total_years = sum(ages)
+#print(youngest, oldest, total_years)
 
+##Slicing a list
+##Getting the first three items
+#finishers = ['kai', 'abe', 'ada', 'gus', 'zoe']
+#first_three = finishers[:3]
+#middle_three = finishers[1:4]
+#last_three = finishers[-3:]
+#copy_of_finishers = finishers[:]
+#print(copy_of_finishers)
+#print(first_three, middle_three, last_three)
+
+##List comprehensions
+##To write a comprehension, define an expression for the values you want to store in the list.
+##Then write a for loop to generate input values needed to make the list.
+
+#squares = []
+#for x in range(1,11):
+#    square = x**2
+#    squares.append(square)
+#print(squares)
+
+##Using a comprehension to generate a list of square numbers
+#squares = [x**2 for x in range(1,11)]
+#print(squares)
+
+#names = ['kai', 'abe', 'ada', 'gus', 'zoe']
+#upper_names = []
+#for name in names:
+#    upper_names.append(name.upper())
+#print(upper_names)
+
+##Using a comprehension to convert a list of names to upper case
+#names = ['kai', 'abe', 'ada', 'gus', 'zoe']
+#upper_names = [name.upper() for name in names]
+#print(upper_names)
+
+##Tuples
+#dimensions = (800, 600)
+#for dimension in dimensions:
+#    print(dimension)
+
+#dimensions = (1200, 900)
+#for dimension in dimensions:
+#    print(dimension)
+
+##pythontutor.com is a great tool for seeing how Python
+##keeps track of the information in a list.
+
+#dogs = []
+#dogs.append('willie')
+#dogs.append('hootz')
+#dogs.append('peso')
+#dogs.append('goblin')
+
+#for dog in dogs:
+#    print("Hello " + dog + "!")
+#print("He loves these dogs!")
+
+#print("\nThese were my first two dogs!")
+#old_dogs = dogs[:2]
+#for old_dog in old_dogs:
+#    print(old_dog)
+
+#del dogs[0]
+#dogs.remove('peso')
+#print(dogs)
+
+"""
+    #Styling code
+    #Readbility counts
+
+    #Use four spaces per indentation level.
+    #Keep lines to 79 characters or fewer.
+    #Use single blank lines to group parts of program visually.
+"""
