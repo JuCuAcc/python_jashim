8879d32a67218a043c5d2a22fa167cc28848812f
MD. Jashim Uddin <ju.cu.acc@outlook.com>
2022-10-31 15:00:58 +0600

    Django

---
+++ python_django/python_django.py
@@ -0,0 +1,9 @@
+
+# What is Django?
+# Django is a web framework which helps you build interactive websites using Python.
+# With Django you define the kind of data your site needs to work with, and you define
+# the ways your users can work with that data.
+
+# Installing Django
+# It's usually best to install Django to a virtual environment, where your project
+# can be isolated from your other Python projects.
